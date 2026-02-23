343ec5aee4d1b785b2b5a5f8565314c30541d58c
Balajimarpally <balajimarpally@gmail.com>
2026-02-23 17:49:18 +0530

    backend of lottery

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Server running ✅" });
+});
+
+app.listen(10000, () => {
+  console.log("🚀 Server started on port 10000");
+});
