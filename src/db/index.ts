2b3502e39a1de5475578b4560e76f79e668ab318
MunagaSravani <munagasravani1@gmail.com>
2026-03-12 11:15:56 +0530

    commit only src/db/index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,16 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
-import dotenv from "dotenv";
-import * as schema from "./schema";
-
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+import * as schema from './schema/index';
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: false ,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
 });
 
 export const db = drizzle(pool, { schema });
+export type DB = typeof db;
