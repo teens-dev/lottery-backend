bb794e55c0993a5db24991bfdd52ff11e9439de3
prasanna <prasannapatta25@gmail.com>
2026-03-13 10:09:24 +0530

    removed node modules

---
+++ src/api/controllers/user.controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { db } from "../../db";
+import { users } from "../../db/schema";
+
+export const getUsers = async (req: Request, res: Response) => {
+  try {
+    const result = await db.select().from(users);
+   
+
+    res.json({
+      success: true,
+      data: result
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Server Error"
+    });
+  }
+};
