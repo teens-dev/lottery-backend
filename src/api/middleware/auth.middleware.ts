243be97be1a10e06dbd2d213524fd43b1460c2cd
Gokaram Kurumoorthy <gkmurthy1996@gmail.com>
2026-03-16 17:43:52 +0530

    added controller, middleware, router, validator files

--- src/api/middleware/auth.middleware.ts
+++ src/api/middleware/auth.middleware.ts
@@ -1,41 +1,35 @@
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 
+export interface AuthRequest extends Request {
+  user?: any;
+}
+
 export const protect = (
-  req: Request,
+  req: AuthRequest,
   res: Response,
   next: NextFunction
 ) => {
-
   const authHeader = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith("Bearer")) {
-
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({
-      message: "Not authorized"
+      success: false,
+      message: "Not authorized. Token missing",
     });
-
   }
 
   const token = authHeader.split(" ")[1];
 
   try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
 
-    const decoded = jwt.verify(
-      token,
-      process.env.JWT_SECRET as string
-    );
-
-    (req as any).user = decoded;
-
+    req.user = decoded;
     next();
-
   } catch {
-
-    res.status(401).json({
-      message: "Invalid token"
+    return res.status(401).json({
+      success: false,
+      message: "Invalid token",
     });
-
   }
-
 };
