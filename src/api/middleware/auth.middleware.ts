456a1a18619b13253dfb3cba931f0a2f20184c05
Gokaram Kurumoorthy <gkmurthy1996@gmail.com>
2026-03-30 16:09:43 +0530

    updated cookies in auth middleware

--- src/api/middleware/auth.middleware.ts
+++ src/api/middleware/auth.middleware.ts
@@ -10,19 +10,20 @@ export const protect = (
   res: Response,
   next: NextFunction
 ) => {
-  const authHeader = req.headers.authorization;
+  const token = req.cookies?.token;
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+  if (!token) {
     return res.status(401).json({
       success: false,
       message: "Not authorized. Token missing",
     });
   }
 
-  const token = authHeader.split(" ")[1];
-
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string
+    );
 
     req.user = decoded;
     next();
