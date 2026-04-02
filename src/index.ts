ae32565e6b80288784e1bb045faaf4a090fe10c1
Balajimarpally <balajimarpally@gmail.com>
2026-04-02 15:21:06 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,13 @@ dotenv.config();
 import walletRoutes from "./api/routes/wallet.routes";
 
 const app = express();
-
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());   // ✅ IMPORTANT FIX
 
